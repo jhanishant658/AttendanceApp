3ebe2ea62d9405f2f577ce1b640f00de17146475
jhanishant658 <jhanishant658@gmail.com>
2025-12-01 22:23:54 +0530

    Configure MySQL datasource and update dependencies
    Replaced the Spring Boot Actuator dependency with the MySQL connector in pom.xml. Updated application.properties to configure MySQL datasource, credentials, JPA settings, and changed the server port to 8081. Reformatted AttendanceAppApplication.java for consistent indentation.

--- src/main/java/com/attendenceApp/AttendanceApp/AttendanceAppApplication.java
+++ src/main/java/com/attendenceApp/AttendanceApp/AttendanceAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AttendanceAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AttendanceAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceAppApplication.class, args);
+    }
 }
