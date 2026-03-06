7f7bc80c08c6342182f750390ecd88a88f3892b2
Nishant jha <jhanishant658@gmail.com>
2026-03-06 17:01:00 +0530

    Update AppConfig.java

--- src/main/java/com/attendenceApp/AttendanceApp/Configuration/AppConfig.java
+++ src/main/java/com/attendenceApp/AttendanceApp/Configuration/AppConfig.java
@@ -16,7 +16,7 @@ public class AppConfig {
 
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
